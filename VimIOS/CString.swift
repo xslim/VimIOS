b253f9f96b4d722aaf09add6ffa673d9d54a5b91
Lars Kindler <larki@users.noreply.github.com>
2015-10-28 23:06:50 -0400

    Vim seems to be running in the background nicely

---
+++ VimIOS/CString.swift
@@ -0,0 +1,39 @@
+//
+//  CStringArray.swift
+//  VimIOS
+//
+// d.
+// from https://gist.github.com/neilpa/b430d148d1c5f4ae5ddd
+
+class CString {
+    private let _len: Int
+    let buffer: UnsafeMutablePointer<Int8>
+    
+    init(_ string: String) {
+        (_len, buffer) = string.withCString {
+            let len = Int(strlen($0) + 1)
+            let dst = strcpy(UnsafeMutablePointer<Int8>.alloc(len), $0)
+            return (len, dst)
+        }
+    }
+    
+    deinit {
+        buffer.dealloc(_len)
+    }
+}
+
+// An array of C-style strings (e.g. char**) for easier interop.
+class CStringArray {
+    // Have to keep the owning CString's alive so that the pointers
+    // in our buffer aren't dealloc'd out from under us.
+    private let _strings: [CString]
+    var pointers: [UnsafeMutablePointer<Int8>]
+    
+    init(_ strings: [String]) {
+        _strings = strings.map { CString($0) }
+        pointers = _strings.map { $0.buffer }
+        // NULL-terminate our string pointer buffer since things like
+        // exec*() and posix_spawn() require this.
+        pointers.append(nil)
+    }
+}
--- VimIOS/VimAppDelegate.swift
+++ VimIOS/VimAppDelegate.swift
@@ -8,17 +8,48 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+
     
-    
-    func applicationDidFinishLaunching(application: UIApplication) {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        var url: NSURL?
         
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { self.VimStarter(url)}
+//        self.performSelectorOnMainThread(NSSelectorFromString("VimStarter"), withObject: url, waitUntilDone: false)
+       // VimStarter(url)
+        return true
+        
+    }
+    
+    
+    
+    func VimStarter(url: NSURL?) {
+        if let vimPath = NSBundle.mainBundle().resourcePath {
+            let runtimePath = vimPath + "runtime"
+            vim_setenv("VIM".char, vimPath.char)
+            vim_setenv("VIMRUNTIME".char, runtimePath.char)
+            
+            let workingDir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+            
+            vim_setenv("HOME".char, workingDir.char)
+            
+            var numberOfArguments = 1
+            var arguments = CStringArray(["vim"]).pointers
+            VimMain(Int32(numberOfArguments),&arguments)
+        }
     }
 
 
 
 }
 
+
+extension String {
+    var char: UnsafeMutablePointer<char_u> {
+        return UnsafeMutablePointer<char_u>((self as NSString).UTF8String)
+    }
+}
